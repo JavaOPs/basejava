d91ae923417cb9819a313aceec4177cbdc09885d
Чимаев Максим <chimax.prog@gmail.com>
2021-03-05 13:35:08 +0300

    Update MainArray.java

--- src/MainArray.java
+++ src/MainArray.java
@@ -13,7 +13,7 @@ public class MainArray {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
-            System.out.print("Введите одну из команд - (list | save uuid | delete uuid | get uuid | clear | exit): ");
+            System.out.print("Введите одну из команд - (list | size | save uuid | delete uuid | get uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 2) {
                 System.out.println("Неверная команда.");
