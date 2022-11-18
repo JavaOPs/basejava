e65ee741603f44fd917b864be83838ccf34540ba
VladimirSafronov <safron5000@mail.ru>
2022-11-18 23:18:57 +0300

    implemented methods in ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,58 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
 
-    void clear() {
-    }
+  Resume[] storage = new Resume[10000];
+  private int storageSize = 0;
 
-    void save(Resume r) {
+  void clear() {
+    for (int i = 0; i < storageSize; i++) {
+      storage[i] = null;
     }
+    storageSize = 0;
+  }
 
-    Resume get(String uuid) {
-        return null;
-    }
+  void save(Resume r) {
+    storage[storageSize++] = r;
+  }
 
-    void delete(String uuid) {
+  Resume get(String uuid) {
+    for (int i = 0; i < storageSize; i++) {
+      if (uuid.equals(storage[i].uuid)) {
+        return storage[i];
+      }
     }
+    return null;
+  }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
-        return new Resume[0];
+  void delete(String uuid) {
+    int deletedIndex = -1;
+    for (int i = 0; i < storageSize; i++) {
+      if (uuid.equals(storage[i].uuid)) {
+        deletedIndex = i;
+        break;
+      }
     }
+    if (deletedIndex >= 0) {
+      for (int i = deletedIndex; i < storageSize - 1; i++) {
+        storage[i] = storage[i + 1];
+      }
+      storageSize -= 1;
+    }
+  }
 
-    int size() {
-        return 0;
+  /**
+   * @return array, contains only Resumes in storage (without null)
+   */
+  Resume[] getAll() {
+    Resume[] onlyWithData = new Resume[storageSize];
+    for (int i = 0; i < storageSize; i++) {
+      onlyWithData[i] = storage[i];
     }
+    return onlyWithData;
+  }
+
+  int size() {
+    return storageSize;
+  }
 }
