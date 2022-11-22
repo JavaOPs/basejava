886a53f0b43b4748a973483082d43e162aba6ec1
VladimirSafronov <safron5000@mail.ru>
2022-11-22 13:20:22 +0300

    introduced AbstractArrayStorage

---
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -0,0 +1,26 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+import java.util.Arrays;
+
+public abstract class AbstractArrayStorage implements Storage {
+  protected static final int STORAGE_LIMIT = 10000;
+  protected Resume[] storage = new Resume[STORAGE_LIMIT];
+  protected int storageSize;
+
+  public int size() {
+    return storageSize;
+  }
+
+  public Resume get(String uuid) {
+    int index = getIndex(uuid);
+    if (index >= 0) {
+      return storage[index];
+    } else {
+      System.out.println("Resume " + uuid + " isn't exists!");
+      return null;
+    }
+  }
+
+  protected abstract int getIndex(String uuid);
+}
