927ea3e362c661f13caf5a64bfdccfe9ee7fff71
VladimirSafronov <safron5000@mail.ru>
2022-11-23 16:38:02 +0300

    created methods save(), delete() in ArrayStorage, SortedArrayStorage
    * used pattern Template method

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -11,4 +11,19 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     searchKey.setUuid(uuid);
     return Arrays.binarySearch(storage, 0, storageSize, searchKey);
   }
+
+  @Override
+  protected void insertElement(Resume r, int index) {
+    int insertIndex = -index - 1;
+    System.arraycopy(storage, insertIndex, storage, insertIndex + 1, storageSize - insertIndex);
+    storage[insertIndex] = r;
+  }
+
+  @Override
+  protected void fillDeletedElement(int index) {
+    int numMoved = storageSize - index;
+    if (numMoved >= 0) {
+      System.arraycopy(storage, index + 1, storage, index, numMoved);
+    }
+  }
 }
