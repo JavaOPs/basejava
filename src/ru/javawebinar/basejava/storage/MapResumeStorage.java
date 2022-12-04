e02c8d95f72d4b05802fda6fa3dd8fe18328d1fe
VladimirSafronov <safron5000@mail.ru>
2022-12-04 18:00:05 +0300

    created MapResumeStorage

--- src/ru/javawebinar/basejava/storage/MapResumeStorage.java
+++ src/ru/javawebinar/basejava/storage/MapResumeStorage.java
@@ -1,52 +1,57 @@
 package ru.javawebinar.basejava.storage;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import ru.javawebinar.basejava.model.Resume;
 
 public class MapResumeStorage extends AbstractStorage {
 
+  Map<String, Resume> mapStorage = new HashMap<>();
+
   @Override
-  protected Object getSearchKey(String uuid) {
-    return null;
+  protected boolean isExist(Object resume) {
+    return resume != null;
   }
 
   @Override
-  protected void doUpdate(Resume r, Object searchKey) {
-
+  protected Resume getSearchKey(String uuid) {
+    return mapStorage.get(uuid);
   }
 
   @Override
-  protected boolean isExist(Object searchKey) {
-    return false;
+  protected void doUpdate(Resume r, Object resume) {
+    mapStorage.put(r.getUuid(), r);
   }
 
   @Override
-  protected void doSave(Resume r, Object searchKey) {
-
+  protected void doSave(Resume r, Object resume) {
+    mapStorage.put(r.getUuid(), r);
   }
 
   @Override
-  protected void doDelete(Object searchKey) {
-
+  protected void doDelete(Object resume) {
+    mapStorage.remove(((Resume) resume).getUuid());
   }
 
   @Override
-  protected Resume doGet(Object searchKey) {
-    return null;
+  protected Resume doGet(Object resume) {
+    return (Resume) resume;
   }
 
   @Override
   protected List<Resume> doCopyAll() {
-    return null;
+    return new ArrayList<>(mapStorage.values());
   }
 
   @Override
   public void clear() {
-
+    mapStorage.clear();
   }
 
   @Override
   public int size() {
-    return 0;
+    return mapStorage.size();
   }
 }
