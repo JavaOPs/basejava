215ac0202540729fc8d8abef59cb45575d2c1590
VladimirSafronov <safron5000@mail.ru>
2023-01-11 12:03:39 +0600

    added contacts to SqlStorage
    * changed db port

---
+++ src/ru/javawebinar/basejava/sql/SqlTransaction.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.basejava.sql;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface SqlTransaction<T> {
+
+  T execute(Connection conn) throws SQLException;
+}
