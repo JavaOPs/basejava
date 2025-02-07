081e3baa9d96e2bcd07acf044e1666f91beca657
Admin <yaf@bk.ru>
2025-02-07 11:31:38 +0500

    HW4: MainReflection_HW

--- src/com/urise/webapp/storage/MainReflection.java
+++ src/com/urise/webapp/storage/MainReflection.java
@@ -9,16 +9,15 @@ import java.lang.reflect.Method;
 public class MainReflection {
     public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
-        Field field = r.getClass().getDeclaredFields()[0];
+        Class <? extends Resume> resumeClass = r.getClass();
+        Field field = resumeClass.getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
         System.out.println(r);
-        Method method = r.getClass().getMethod("toString");
-        method.invoke(r);
-        System.out.println(method.getClass());
-        System.out.println(method.getName());
-        System.out.println(method);
+        Method method = resumeClass.getMethod("toString");
+        Object result = method.invoke(r);
+        System.out.println(result);
     }
 }
