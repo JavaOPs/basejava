c9519eef31f26f25ce6d218d77fe718cd0c80f41
Admin <yaf@bk.ru>
2025-02-11 17:36:06 +0500

    HW5: static initializer block

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -13,17 +13,27 @@ import static org.junit.Assert.assertThrows;
 
 public abstract class AbstractArrayStorageTest {
 
-    private static final String UUID_1 = "uuid1";
-    private static final Resume RESUME_1 = new Resume(UUID_1);
-    private static final String UUID_2 = "uuid2";
-    private static final Resume RESUME_2 = new Resume(UUID_2);
-    private static final String UUID_3 = "uuid3";
-    private static final Resume RESUME_3 = new Resume(UUID_3);
-    private static final String UUID_4 = "uuid4";
-    private final Resume SAVE_RESUME = new Resume(UUID_4);
-    private final Resume UUID_NOT_EXIST = new Resume("dummy");
+    private Storage storage;
 
-    private final Storage storage;
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+    private static final String UUID_4 = "uuid4";
+
+    private static final Resume RESUME_1;
+    private static final Resume RESUME_2;
+    private static final Resume RESUME_3;
+    private static final Resume SAVE_RESUME;
+
+    static {
+        RESUME_1 = new Resume(UUID_1);
+        RESUME_2 = new Resume(UUID_2);
+        RESUME_3 = new Resume(UUID_3);
+        SAVE_RESUME = new Resume(UUID_4);
+    }
+
+      private final Resume UUID_NOT_EXIST = new Resume("dummy");
+
     private final int size = 3;
 
     protected AbstractArrayStorageTest(Storage storage) {
